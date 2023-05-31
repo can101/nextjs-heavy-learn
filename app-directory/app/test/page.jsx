0e8d041618d291d1ae210c52d2890f9d6b6e16ac
can101 <cancelikdev@outlook.com>
2023-06-01 02:04:56 +0300

    add: loading and error component

--- app-directory/app/test/page.jsx
+++ app-directory/app/test/page.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
-export default function TestPage(){
+export default function TestPage({searchParams}){
+    if(searchParams.error=="true"){
+        throw new Error("this is an error");
+    }
     return <div>Test Page</div>
 }
